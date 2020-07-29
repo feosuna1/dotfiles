130a958bf681b47cad3fddf90a196915c3018531
Enrique Osuna <enrique.osuna@gmail.com>
2020-07-29 15:59:18 -0700

    Optimize repo left and right prompts
    Prompt for `hg` repos were hanging. This refactors the `prompt.fish`
    script to a bit more easier while attempting to ease any burden on `hg`.

--- files/config/fish/conf.d/prompt.fish
+++ files/config/fish/conf.d/prompt.fish
@@ -5,51 +5,56 @@ end
 function fish_right_prompt
    if not set -q -g __fish_freak_magic_functions_defined
         set -g __fish_freak_magic_functions_defined
-        function _git_branch_name
-            set -l branch (git symbolic-ref --quiet HEAD ^/dev/null)
-            if set -q branch[1]
-                echo (string replace -r '^refs/heads/' '' $branch)
+
+        function __git_branch_name
+            set -l branch (git branch --show-current 2> /dev/null)
+            if [ $branch ]
+                echo $branch
             else
-                echo (git rev-parse --short HEAD ^/dev/null)
+                git rev-parse --short HEAD 2> /dev/null
             end
         end
 
-        function _is_git_dirty
-            echo (git status -s --ignore-submodules=dirty ^/dev/null)
+        function __is_git_dirty
+            if not git diff-files --no-ext-diff --quiet
+                echo 1
+            else if not git diff-index --no-ext-diff --quiet --cached HEAD
+                echo 2
+            else if [ (git ls-files --other --exclude-standard 2> /dev/null) ]
+                echo 3
+            end
         end
 
-        function _is_git_repo
-            type -q git
-            or return 1
-            git status -s >/dev/null ^/dev/null
+        function __is_git_repo
+            type -q git or return 1
+            git rev-parse --is-inside-work-tree > /dev/null 2> /dev/null
         end
 
-        function _hg_branch_name
-            echo (hg branch ^/dev/null)
+        function __hg_branch_name
+            hg bookmarks 2> /dev/null | grep \* | awk '{print $2}'
         end
 
-        function _is_hg_dirty
-            echo (hg status -mard ^/dev/null)
+        function __is_hg_dirty
+            hg status -A 2> /dev/null
         end
 
-        function _is_hg_repo
-            type -q hg
-            or return 1
-            hg summary >/dev/null ^/dev/null
+        function __is_hg_repo
+            type -q hg or return 1
+            hg root > /dev/null 2> /dev/null
         end
 
         function _repo_branch_name
-            eval "_$argv[1]_branch_name"
+            eval "__$argv[1]_branch_name"
         end
 
         function _is_repo_dirty
-            eval "_is_$argv[1]_dirty"
+            eval "__is_$argv[1]_dirty"
         end
 
         function _repo_type
-            if _is_hg_repo
+            if __is_hg_repo
                 echo 'hg'
-            else if _is_git_repo
+            else if __is_git_repo
                 echo 'git'
             end
         end
@@ -57,36 +62,38 @@ function fish_right_prompt
 
     set -l repo_type (_repo_type)
     if [ $repo_type ]
-        set -l branch_color (set_color -o $fish_color_cwd)
         set -l repo_branch (_repo_branch_name $repo_type)
-        set repo_info "$branch_color [$repo_branch]$normal"
 
         if [ (_is_repo_dirty $repo_type) ]
-            set -l status_color (set_color -o $fish_color_error)
-            set -l dirty $status_color'✗'
-            set repo_info $dirty$repo_info
+            set_color -o $fish_color_error
+            echo '✗ '
         end
-        echo -n -s $repo_info
+    
+        set_color -o $fish_color_cwd
+        echo -s '[' $repo_branch ']' 
+        set_color normal
     end
 end
 
 function fish_prompt
-    set -l __last_command_exit_status $status
-    set -l normal (set_color -o normal)
-    set -l cwd_color (set_color -o $fish_color_cwd)
-    set -l cwd_root_color (set_color -o $fish_color_cwd_root)
-    set -l success_color (set_color -o $fish_color_status)
-    set -l failure_color (set_color -o $fish_color_error)
-    set -l separator_color (set_color -o $fish_color_comment)
+    set -l last_status $status
 
-    set -l cwd $cwd_color(basename (prompt_pwd))
+    # Separator
+    set_color -o $fish_color_comment
+    echo '————————————————————————————————————————————————————————————'
 
-    set -l cursor_color $success_color
-    if test $__last_command_exit_status != 0
-        set cursor_color $failure_color
+    # Current Working Directory
+    set_color $fish_color_cwd
+    echo -n (basename (prompt_pwd))
+
+    # Cursor highlighted with last exit status
+    if [ $last_status = 0 ]
+        set_color -o $fish_color_status
+        echo -n ' ⪢ '
+    else
+        set_color -o $fish_color_error
+        echo -n ' ⩔ '
     end
-    set -l cursor " $cursor_color»$normal"
 
-    echo -s $separator_color "------------------------------------------------------------"
-    echo -n -s $cwd $cursor ' '
+    set_color normal
 end
