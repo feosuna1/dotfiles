ffb66858c9998ff304ef5c1e48d2c3dea51daf2e
Enrique Osuna <12620+feosuna1@users.noreply.github.com>
2026-02-07 12:12:15 +0200

    Deconstruct install.sh into smaller scripts

--- install.sh
+++ install.sh
@@ -1,59 +1,15 @@
-#!/usr/bin/env bash
-#==================
+#!/bin/bash
 set -o errexit # Exit if any individual command fails and isn't handled
 set -o nounset # Exit if unset variables are used
 
 if [ -n "${BASH_VERSION:-}" ]; then
-    # If any command in a pipeline fails, the whole pipeline should fail.
-    # This option isn't define by POSIX `sh` but is available if we're running
-    # as `bash`.
-    # Disable warning for using non-portable option (SC2039 for shellcheck <=
-    # 0.7.1, SC3040 for >= 0.7.2).
-    # shellcheck disable=SC2039,SC3040
-    set -o pipefail
+    # shellcheck disable=SC3040
+    set -o pipefail # Exit if any command in a pipeline fails and isn't handled
 fi
 
-kill_sudo_on_exit() {
-    trap - EXIT
-
-    # Invalidate the cached credentials, if we're the ones that cached it
-    if [[ "$SUDO_CREDENTIALS_CACHED_IN_SCRIPT" -ne "0" ]]; then
-        sudo -k
-    fi
-
-    # Kill all subprocesses
-    kill -9 $(jobs -rp) 2> /dev/null
-    wait $(jobs -rp) 2>/dev/null
-}
-trap kill_sudo_on_exit EXIT
-
-ask_for_sudo_while_script_runs() {
-    # Check to see the credentials were already cached before we got here.
-    # When we exit, we want to make sure we only invalidate the credentials
-    # if we were the ones that caused it to be cached.
-    sudo -nv 2> /dev/null || rv=$?
-    SUDO_CREDENTIALS_CACHED_IN_SCRIPT="$rv"
-
-    sudo -v -p "Enter your password to install: "
-
-    # This will create a background subprocess that will extends
-    # sudo timeout every 5 seconds.
-    (
-        while sudo -nv 2> /dev/null; do
-            sleep 5
-        done
-    )&
-}
-
-set_defaults_if_missing() {
-    local domain=$1
-    local key=$2
-    local current_value=$(defaults read "$domain" "$key" 2> /dev/null || echo "")
-    if [[ -z "$current_value" ]]; then
-        shift 2
-        defaults write "$domain" "$key" "$@"
-    fi
-}
+# shellcheck source=files/sudo-support
+source "$HOME/.dotfiles/files/sudo-support"
+BIN_DIR="$HOME/.dotfiles/files/bin"
 
 make_and_install_roots() {
     TARGET_ROOT_DIR="$HOME"
@@ -61,7 +17,7 @@ make_and_install_roots() {
     COPY_DIR="$SOURCE_DIR/copy/User"
     SYMLINK_DIR="$SOURCE_DIR/symlinks/User"
 
-    find "$SYMLINK_DIR/" -type f \( ! -name ".DS_Store" \) | while read -d $'\n' symlink; do
+    find "$SYMLINK_DIR/" -type f \( ! -name ".DS_Store" \) | while read -r -d $'\n' symlink; do
         symlink=${symlink#*//}
         if [[ "$symlink" == *".symlink" ]]; then
             # A symlink file contains a single line representing the target to symlink to
@@ -81,113 +37,18 @@ make_and_install_roots() {
     done
 
     rsync -razI --ignore-existing --progress "$COPY_DIR/" "$HOME"
+
+    # shellcheck disable=all
     [[ -f ~/.zshenv ]] && source ~/.zshenv
+
+    # shellcheck disable=all
     [[ -f ~/.zprofile ]] && source ~/.zprofile
+
+    # shellcheck disable=all
     [[ -f ~/.zshrc ]] && source ~/.zshrc
 }
 
-# -- Bootstrapping -------------------------------------------------------------
-
-install_homebrew() {
-    if command -v brew >/dev/null; then
-        # Homebrew already installed.
-        echo "Skipping homebrew, already installed."
-    else
-        echo "Installing homebrew and packages..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
-
-    brew analytics off || true
-    brew update
-    brew bundle install
-}
-
-configure_defaults() {
-    # The following has been largely inspired by: https://github.com/mathiasbynens/dotfiles/blob/main/.macos
-
-    # Close any open System Preferences panes, to prevent them from overriding settings being changed
-    osascript -e 'tell application "System Preferences" to quit'
-
-    # Enable dark mode, the following is set during Mac Buddy, we need to overwrite the value instead
-    defaults write NSGlobalDomain AppleInterfaceStyle Dark
-
-    # Show file extensions by default
-    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-    # Expand save panel by default
-    set_defaults_if_missing NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-    set_defaults_if_missing NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-    # Save to disk (not to iCloud) by default
-    set_defaults_if_missing NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-    # Expand print panel by default
-    set_defaults_if_missing NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-    set_defaults_if_missing NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-    # Automatically quit printer app once the print jobs complete
-    set_defaults_if_missing com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-    # Disable the “Are you sure you want to open this application?” dialog
-    set_defaults_if_missing com.apple.LaunchServices LSQuarantine -bool false
-
-    # Trackpad, mouse, keyboard, Bluetooth accessories, and input
-    # The following settings are seeded by the operating system, so we need to overwrite the values instead
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking 0
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad DragLock 0
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging 0
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick 0
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFiveFingerPinchGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerPinchGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadHandResting 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadHorizScroll 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadMomentumScroll 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadPinch 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRotate 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadScroll 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag 0
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture 2
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture 1
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture 3
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad USBMouseStopsTrackpad 0
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad UserPreferences 1
-
-    # Use list view in all Finder windows by default
-    set_defaults_if_missing com.apple.finder FXPreferredViewStyle -string "Nlsv"
-    set_defaults_if_missing com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-    # Enable AirDrop over Ethernet and on unsupported Macs running Lion
-    set_defaults_if_missing com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-
-    # Show the ~/Library folder
-    chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
-
-    # Show the /Volumes folder
-    sudo chflags nohidden /Volumes
-}
-
-configure_dns_server() {
-    cloudflare_dns="1.1.1.1 1.0.0.1"
-    cloudflare_search_domains="2606:4700:4700::1111 2606:4700:4700::1001"
-
-    #  The following was inspired by https://superuser.com/questions/86184/change-dns-server-from-terminal-or-script-on-mac-os-x
-    services=$(networksetup -listallnetworkservices | grep 'Wi-Fi\|Ethernet\|USB')
-    for service in $services; do
-        networksetup -setdnsservers "$service" empty
-        networksetup -setdnsservers "$service" $cloudflare_dns
-
-        networksetup -setsearchdomains "$service" empty
-        networksetup -setsearchdomains "$service" $cloudflare_search_domains
-    done
-}
-
-ask_for_sudo_while_script_runs
 make_and_install_roots
-install_homebrew
-configure_defaults
-configure_dns_server
+"$BIN_DIR/install-brew"
+"$BIN_DIR/configure-user-defaults"
+"$BIN_DIR/configure-dns-servers"
